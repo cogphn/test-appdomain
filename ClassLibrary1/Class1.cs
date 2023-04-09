45ad13168c3f8e9b9f6398fd058536094b3cbda1
Jason Deyalsingh <jason.deyalsingh@gmail.com>
2023-04-09 11:14:06 -0400

    init

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public sealed class Diagnostics : AppDomainManager {
+    public override void InitializeNewDomain(AppDomainSetup appDomainInfo) {
+        Console.WriteLine("test1");
+        bool result = EvilClass.Run();
+        return;
+    }
+}
+
+
+public class EvilClass {
+    public static bool Run() {
+        Console.WriteLine("EVIL HAX!!");
+        return true;
+    }
+}
---
+++ Launcher/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Launcher {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Starting...");
+            long x = 9223372036824775807;
+            var a = x / 3;
+
+            Console.WriteLine("Done.");
+        }
+    }
+}
